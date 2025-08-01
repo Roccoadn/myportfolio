eef4d4db30da02596ed9589431319b2bfdec68e0
Roccoadn <roccoandreone@gmail.com>
2025-08-01 16:25:33 -0300

    fix again

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -7,7 +7,7 @@ AOS.init();
 function Techs(){
     return(
         <section id='techs' data-aos="fade-up" data-aos-duration="900">
-            <h1><BoltIcon/>Tecnologías y herramientas</h1>
+            <h1><BoltIcon/>Tecnologías <br />y <br /> herramientas</h1>
             <div className="techs-container">
                 <ul>
                     <li>
